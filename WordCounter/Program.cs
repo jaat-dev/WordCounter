aa1cca0b0509e6e6752a52b425af956955d3cd0c
jaat26 <john_arevalo@hotmail.com>
2021-06-12 22:15:26 -0500

    Create Project

---
+++ WordCounter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WordCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> wordCounter = new Dictionary<string, string>();
+            wordCounter.Add("txt", "notepad.exe");
+            wordCounter.Add("bmp", "paint.exe");
+            wordCounter.Add("dib", "paint.exe");
+            wordCounter.Add("rtf", "wordpad.exe");
+            Console.WriteLine(wordCounter);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
